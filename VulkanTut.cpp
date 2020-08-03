b9c5363fb3f821155d24527e6349e1d590f12365
Hjalte Dalland <hjda@itu.dk>
2020-08-03 17:34:40 +0200

    Logical Devices and Queues

--- VulkanTut.cpp
+++ VulkanTut.cpp
@@ -3,7 +3,6 @@
 
 #include <iostream>
 #include <functional>
-#include <algorithm>
 #include <stdexcept>
 #include <cstdlib>
 #include <map>
@@ -71,6 +70,8 @@ private:
 	
 	VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
 	VkDevice device;
+
+	VkQueue graphicsQueue;
 
 	void initWindow() {
 		glfwInit();
@@ -320,7 +321,42 @@ private:
 		return score;
 	}
 
-	void createLogicalDevice();
+	void createLogicalDevice() {
+		QueueFamilyIndices indices = findQueueFamilies(physicalDevice);
+
+		VkDeviceQueueCreateInfo queueCreateInfo{};
+		queueCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+		queueCreateInfo.queueFamilyIndex = indices.graphicsFamily.value();
+		queueCreateInfo.queueCount = 1;
+
+		//Queue Priority is required even with only one queue. Range: 0.0 - 1.0
+		float queuePriority = 1.0f;
+		queueCreateInfo.pQueuePriorities = &queuePriority;
+
+		VkPhysicalDeviceFeatures deviceFeatures{};
+
+		VkDeviceCreateInfo createInfo{};
+		createInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+		createInfo.pQueueCreateInfos = &queueCreateInfo;
+		createInfo.queueCreateInfoCount = 1;
+
+		createInfo.pEnabledFeatures = &deviceFeatures;
+
+		createInfo.enabledExtensionCount = 0;
+
+		if(enableValidationLayers) {
+			createInfo.enabledLayerCount = static_cast<uint32_t>(validationLayers.size());
+			createInfo.ppEnabledLayerNames = validationLayers.data();
+		} else {
+			createInfo.enabledLayerCount = 0;
+		}
+
+		if (vkCreateDevice(physicalDevice, &createInfo, nullptr, &device) != VK_SUCCESS) {
+			throw std::runtime_error("Failed to create logical device!");
+		}
+
+		vkGetDeviceQueue(device, indices.graphicsFamily.value(), 0, &graphicsQueue);
+	}
 
 	void mainLoop() {
 		while (!glfwWindowShouldClose(window)) {
@@ -329,6 +365,8 @@ private:
 	}
 
 	void cleanup() {
+		vkDestroyDevice(device, nullptr);
+		
 		if (enableValidationLayers) {
 			DestroyDebugUtilsMessengerEXT(instance, debugMessenger, nullptr);
 		}

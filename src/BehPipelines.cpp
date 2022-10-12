f511161fbfd8565a8010d469105c214c7e782166
Hjalte Sorgenfrei Mac Dalland <hjaltesorgenfrei@gmail.com>
2022-10-12 14:04:34 +0200

    Don't recreate pipelines when resizing

--- src/BehPipelines.cpp
+++ src/BehPipelines.cpp
@@ -37,28 +37,6 @@ BehPipeline::BehPipeline(std::shared_ptr<BehDevice>& device, PipelineConfigurati
             .primitiveRestartEnable = VK_FALSE
     };
 
-    vk::Viewport viewport {
-            .x = 0.0f,
-            .y = 0.0f,
-            .width = static_cast<float>(config.extent.width),
-            .height = static_cast<float>(config.extent.height),
-            .minDepth = 0.0f,
-            .maxDepth = 1.0f
-    };
-
-    vk::Rect2D scissor {
-            .offset = {0, 0},
-            .extent = config.extent
-    };
-
-    vk::PipelineViewportStateCreateInfo viewportState {
-            .viewportCount = 1,
-            .pViewports = &viewport,
-            .scissorCount = 1,
-            .pScissors = &scissor,
-    };
-
-
     vk::PipelineRasterizationStateCreateInfo rasterizer {
             .depthClampEnable = VK_FALSE,
             .rasterizerDiscardEnable = VK_FALSE,
@@ -109,6 +87,16 @@ BehPipeline::BehPipeline(std::shared_ptr<BehDevice>& device, PipelineConfigurati
             .stencilTestEnable = VK_FALSE
     };
 
+    std::vector<vk::DynamicState> dynamicStates {
+        vk::DynamicState::eScissor,
+        vk::DynamicState::eViewport
+    };
+
+    vk::PipelineDynamicStateCreateInfo dynamicStateCreateInfo {
+        .dynamicStateCount = static_cast<uint32_t>(dynamicStates.size()),
+        .pDynamicStates = dynamicStates.data()
+    };
+
     vk::GraphicsPipelineCreateInfo pipelineInfo {
             // Reference programmable stages
             .stageCount = static_cast<uint32_t>(shaderStageCreateInfos.size()),
@@ -117,11 +105,11 @@ BehPipeline::BehPipeline(std::shared_ptr<BehDevice>& device, PipelineConfigurati
             .pStages = shaderStageCreateInfos.data(),
             .pVertexInputState = &vertexInputInfo,
             .pInputAssemblyState = &inputAssembly,
-            .pViewportState = &viewportState,
             .pRasterizationState = &rasterizer,
             .pMultisampleState = &multisampling,
             .pDepthStencilState = &depthStencil,
             .pColorBlendState = &colorBlending,
+            .pDynamicState = &dynamicStateCreateInfo,
 
             // A vulkan handle, not a struct pointer
             .layout = config.pipelineLayout,

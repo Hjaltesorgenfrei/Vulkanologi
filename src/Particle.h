6e85a71a2e30d4015d089737926103a2528062f6
Hjalte Sorgenfrei Mac Dalland <hjaltesorgenfrei@gmail.com>
2023-02-18 22:17:55 +0100

    Add missing files

---
+++ src/Particle.h
@@ -0,0 +1,40 @@
+#ifndef VULKANOLOGI_PARTICLE_H
+#define VULKANOLOGI_PARTICLE_H
+
+#include <glm/glm.hpp>
+#include <vulkan/vulkan.hpp>
+
+struct Particle {
+    glm::vec2 position;
+    glm::vec2 velocity;
+    glm::vec4 color;
+
+    static std::vector<vk::VertexInputBindingDescription> getBindingDescription() {
+        return {
+            vk::VertexInputBindingDescription{
+                .binding = 0,
+                .stride = sizeof(Particle),
+                .inputRate = vk::VertexInputRate::eVertex
+            }
+        };
+    }
+
+    static std::vector<vk::VertexInputAttributeDescription> getAttributeDescriptions() {
+        return {
+            vk::VertexInputAttributeDescription{
+                .location = 0,
+                .binding = 0,
+                .format = vk::Format::eR32G32Sfloat,
+                .offset = offsetof(Particle, position)
+            },
+            vk::VertexInputAttributeDescription{
+                .location = 0,
+                .binding = 1,
+                .format = vk::Format::eR32G32Sfloat,
+                .offset = offsetof(Particle, color)
+            },
+        };
+    }
+};
+
+#endif //VULKANOLOGI_PARTICLE_H

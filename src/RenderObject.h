549702001fe68a67fb0577cd6f1a0a31cf5e65c2
Hjalte Sorgenfrei Mac Dalland <hjaltesorgenfrei@gmail.com>
2023-03-22 23:09:27 +0100

    Clean up some files a bit

--- src/RenderObject.h
+++ src/RenderObject.h
@@ -11,5 +11,4 @@ public:
     Material material;
     MeshPushConstants transformMatrix;
     RenderObject(std::shared_ptr<Mesh> mesh, Material material);
-    void Draw(vk::CommandBuffer commandBuffer);
 };
